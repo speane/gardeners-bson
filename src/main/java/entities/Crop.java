c5bd1497f5026c5d2929609056beb2ecdae40b20
speane <avtolubitel225@gmail.com>
2016-04-01 23:44:43 +0300

    Added more classes to the hierarchy and changed form

---
+++ src/main/java/entities/Crop.java
@@ -0,0 +1,7 @@
+package entities;
+
+/**
+ * Created by Evgeny Shilov on 01.04.2016.
+ */
+public class Crop {
+}
--- src/main/java/entities/Farm.java
+++ src/main/java/entities/Farm.java
@@ -4,4 +4,16 @@ package entities;
  * Created by Evgeny Shilov on 01.04.2016.
  */
 public class Farm {
+    public Stock stock;
+    public Garden garden;
+
+    public Farm() {
+        this.stock = new Stock();
+        this.garden = new Garden();
+    }
+
+    public Farm(Stock stock, Garden garden) {
+        this.stock = stock;
+        this.garden = garden;
+    }
 }
